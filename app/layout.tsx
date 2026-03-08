3bd5a61d6c83c2934818780f7cd213db72856237
TonyNguyenVn17 <17thien.nguyen@gmail.com>
2026-03-07 17:57:05 -0800

    Set global base font size to 18px in CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
         <ThemeScript />
       </head>
-      <body className="font-sans antialiased pb-24 text-lg">
+      <body className="font-sans antialiased pb-24">
         <ThemeProvider>
           <VideoBackground />
           <main className="relative z-1">
